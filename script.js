18190ed91287afb35f2e7e352136027bfe0b1248
nonni <nonnieywahome@gmail.com>
2024-03-02 22:37:47 -0600

    middle branch

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ window.addEventListener("load", () => {
     
     console.log('viewport');
 })
+
+
+// Get the canvas element
+const ctx = document.getElementById('myChart').getContext('2d');
+
+// Create the chart
+const myChart = new Chart(ctx, {
+    type: 'line', // Specify the type of chart (line, bar, pie, etc.)
+    data: chartData, // Pass the chart data
+    options: chartOptions // Pass the chart options
+});
