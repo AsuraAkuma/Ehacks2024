fc15b9eda07e61eaf3673ba70f84d02d96f5b3cf
AsuraAkuma <aigaigkc@gmail.com>
2024-03-03 00:08:33 -0600

    .

--- Backend/generators.cpp
+++ Backend/generators.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include "getText.cpp"
+// #include <emscripten.h>
 using namespace std;
 
 string grabFromList(string *list, int listSize)
@@ -10,12 +11,23 @@ string grabFromList(string *list, int listSize)
     return list[num];
 }
 
-int main()
-{
-    string *list = new string[112];
-    list = getResponses();
-    string response = grabFromList(list, 112);
-    cout << "Response: " << response << endl;
-    delete[] list;
-    return 0;
-}
+// int main()
+// {
+//     string *list = new string[112];
+//     list = getResponses();
+//     string response = grabFromList(list, 112);
+//     cout << "Response: " << response << endl;
+//     delete[] list;
+//     return 0;
+// }
+
+// sample.cpp
+
+// extern "C"
+// {
+//     EMSCRIPTEN_KEEPALIVE
+//     int add(int a, int b)
+//     {
+//         return a + b;
+//     }
+// }
