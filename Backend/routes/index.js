c8497bfa53a074163c05393860282fa4c43741e6
AsuraAkuma <aigaigkc@gmail.com>
2024-03-03 04:46:00 -0600

    .

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const auth = require('./auth');
+const data = require('./data');
 router.use('/auth', auth);
+router.use('/data', data);
 
 module.exports = router;
